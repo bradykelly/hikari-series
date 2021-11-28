65d3189ed1af05bfb2e5ad6ea2fd4709a1c61fbc
Brady Kelly <brady@bradykelly.net>
2021-11-28 16:50:28 +0200

    Shutdown working

--- tutorial/bot.py
+++ tutorial/bot.py
@@ -3,3 +3,8 @@ import os
 import hikari
 import lightbulb
 
+
+class Bot(lightbulb.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.hikari = hikari.Hikari(self.config)
